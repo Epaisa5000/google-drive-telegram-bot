7f8400ff3eef384fa0e496a13d6d90dc81536db4
Epaisa5000 <86190725+Epaisa5000@users.noreply.github.com>
2021-06-28 11:22:59 +0530

    Create config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 class config:
-    BOT_TOKEN = "1826187868:AAHvQdAsJookcxQmngQ7ol7hsstE26C_MyM"
+    BOT_TOKEN = "1716565100:AAHWwRc26cii8MkuZZTbBmIC43gGL_zA0Pk"
     APP_ID = "6042164"
     API_HASH = "f8d04f26224f9750cb35b65d695dc26d"
     DATABASE_URL = "postgres://bksppextfzpbbr:8d0f02f2ddab324260c2d32d481a27214a8c48b43c4a245c8ff6cecf5970f2ef@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d7q5nsh4ovim0"
-    SUDO_USERS = "1841743988 1093654771" # Sepearted by space.
+    SUDO_USERS = "1841743988 1093654771 1874964588" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/joinchat/iOtb4w0iLO05MTgx"
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = "102943132942-4p2asii0omsp46e27r71de9rtla5ms0q.apps.googleusercontent.com"
