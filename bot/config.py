249642afc056ba2dcaac5bb5d6dd21842ff51506
Epaisa5000 <86190725+Epaisa5000@users.noreply.github.com>
2021-07-27 10:01:23 +0530

    Create config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "1716565100:AAHWwRc26cii8MkuZZTbBmIC43gGL_zA0Pk"
     APP_ID = "6042164"
     API_HASH = "f8d04f26224f9750cb35b65d695dc26d"
-    DATABASE_URL = "postgres://bksppextfzpbbr:8d0f02f2ddab324260c2d32d481a27214a8c48b43c4a245c8ff6cecf5970f2ef@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d7q5nsh4ovim0"
+    DATABASE_URL = "postgres://cigzvwimquyjis:0a07130a4387bb716c8583c48e1301671b087fb20e16ab862ca925cbecc6bb5d@ec2-3-222-127-167.compute-1.amazonaws.com:5432/d82qjnfimevspe"
     SUDO_USERS = "1841743988 1093654771 1803009360" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/joinchat/iOtb4w0iLO05MTgx"
     DOWNLOAD_DIRECTORY = "./downloads/"
